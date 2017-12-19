00dea6263b2f3228ae6a2aaaa4750d194b0e581a
Simon Brand <tartanllama@gmail.com>
2017-12-19 15:28:16 +0000

    More constexpr fixes

--- tl/expected.hpp
+++ tl/expected.hpp
@@ -1393,7 +1393,7 @@ public:
       class U = T,
       detail::enable_if_t<std::is_convertible<U &&, T>::value> * = nullptr,
       detail::expected_enable_forward_value<T, E, U> * = nullptr>
-  constexpr expected(U &&v) : expected(in_place, std::forward<U>(v)) {}
+  TL_EXPECTED_MSVC2015_CONSTEXPR expected(U &&v) : expected(in_place, std::forward<U>(v)) {}
 
   template <
       class U = T,

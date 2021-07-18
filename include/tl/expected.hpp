5b3afaf56a628c657925ef9d13a1969e431639b8
Nicolas Fella <nicolas.fella@gmx.de>
2021-07-18 17:35:39 +0200

    Add SPDX license header
    This is used by tools like 'reuse lint' to help managing license compliance.

--- include/tl/expected.hpp
+++ include/tl/expected.hpp
@@ -11,6 +11,9 @@
 // You should have received a copy of the CC0 Public Domain Dedication
 // along with this software. If not, see
 // <http://creativecommons.org/publicdomain/zero/1.0/>.
+//
+// SPDX-License-Identifier: CC-PDDC
+// SPDX-FileCopyrightText: None
 ///
 
 #ifndef TL_EXPECTED_HPP

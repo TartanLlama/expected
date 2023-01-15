6cdc515003f5c1d33d0abbc59a56bb4fd7d23b1f
andy-byers <andrew.byers@utdallas.edu>
2023-01-15 15:12:35 -0600

    Fixed a build issue on Visual Studio 2017. [[maybe_unused]] is not available.

--- include/tl/expected.hpp
+++ include/tl/expected.hpp
@@ -199,13 +199,14 @@ static constexpr unexpect_t unexpect{};
 
 namespace detail {
 template <typename E>
-[[noreturn]] TL_EXPECTED_11_CONSTEXPR void throw_exception([[maybe_unused]] E &&e) {
+[[noreturn]] TL_EXPECTED_11_CONSTEXPR void throw_exception(E &&e) {
 #ifdef TL_EXPECTED_EXCEPTIONS_ENABLED
   throw std::forward<E>(e);
 #else
 #ifdef _MSC_VER
   __assume(0);
 #else
+  (void)e;
   __builtin_unreachable();
 #endif
 #endif
